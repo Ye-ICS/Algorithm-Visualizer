485c9977a4096a48dc42afacbe7d11f42a6e43a3
Kalalily <kalauay@icloud.com>
2025-03-05 12:46:23 -0500

    Create createBoard() and placeNum()

--- Sudoku.java
+++ Sudoku.java
@@ -1,3 +1,13 @@
 public class Sudoku {
-    
+    int[][] board = new int[9][9];
+    public int[][] createBoard(){
+        
+
+        return board;
+    }
+
+    public int placeNum(){
+        int num = 0;
+        return num;
+    }
 }
