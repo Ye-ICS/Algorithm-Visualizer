05e0af25d7d2c2a95a132268aa6313363664c2b7
Kalalily <kalauay@icloud.com>
2025-03-05 13:11:24 -0500

    Create readBoard()
    read board from a text file

--- Sudoku.java
+++ Sudoku.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Sudoku {
     int[][] board = new int[9][9];
     public int[][] createBoard(){
         
-
         return board;
+    }
+
+    public int readBoard(String filename) throws FileNotFoundException{
+        Scanner in = new Scanner(new File(filename));
+        int num = 0;
+        return num;
     }
 
     public int placeNum(){
