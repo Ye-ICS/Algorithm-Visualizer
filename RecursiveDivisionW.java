41d452906d425d4dd87b398211ddeb1464867dfb
ShadowJaguar1 <willdelouche3@gmail.com>
2025-03-06 12:21:40 -0500

    Set initial level of Maze
    Added basic int values, borrowed from the Fractals project.

--- RecursiveDivisionW.java
+++ RecursiveDivisionW.java
@@ -1,5 +1,17 @@
+import javafx.scene.canvas.GraphicsContext;
+
 public class RecursiveDivisionW {
-    RecursiveDivisionW(){
-        //
+    int level = 0;
+    int x = 0;
+    int y = 0;
+    int size = 0;
+    public void drawMaze(GraphicsContext graphicsContext) {
+        
+        if (level <= 0) {
+
+            graphicsContext.fillRect(x, y, size, size);
+
+            return; // return early (skip rest of this method)
+        }
     }
 }
