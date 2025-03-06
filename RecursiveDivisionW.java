b341c591888179dc5db71edd43c81727049aef2e
ShadowJaguar1 <willdelouche3@gmail.com>
2025-03-06 12:48:14 -0500

    compiled a organized plan of atttack

--- RecursiveDivisionW.java
+++ RecursiveDivisionW.java
@@ -6,6 +6,16 @@ public class RecursiveDivisionW {
             graphicsContext.fillRect(x, y, size, size);
             return; //skip rest of method
         }
-        
+
     }
 }
+
+
+
+// needs to make interior walls
+// needs to make the two openings, one per wall
+// need to go down into 1 of the 4 new quadrants
+// need to repeat the process
+// need to stop when when its like 3 or 4 levels in
+// needs to go back then do same for any spaces in the original subchamber
+// then do same for rest of the 4 original subchambers
