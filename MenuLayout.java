aa9681d1067bd455542997bfab6ac79874b38f1d
Declan-gaelen <pm@declanjones.ca>
2025-03-06 13:06:31 -0500

    Add button to main menu for MazeSolver

--- MenuLayout.java
+++ MenuLayout.java
@@ -25,7 +25,10 @@ class MenuLayout extends VBox {
         Button bubbleSortBtn = new Button("Bubble Sort");
         bubbleSortBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new BubbleSortLayout()));
 
-        buttonsBox.getChildren().addAll(bubbleSortBtn);
+        Button mazeSortBtn = new Button("Maze Sort");
+        mazeSortBtn.setOnAction(event -> DeclanJones.main(null));
+
+        buttonsBox.getChildren().addAll(bubbleSortBtn, mazeSortBtn);
         getChildren().addAll(title, buttonsBox);
     }
 }
