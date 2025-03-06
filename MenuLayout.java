1c2df3a564ad8c303f81649b7f0ac48fa7241fd5
Rudraksha-code <rm1ov07@limestone.on.ca>
2025-03-06 12:44:43 -0500

    Basic sudoku grid layout
    Added a menu button for sudoku

--- MenuLayout.java
+++ MenuLayout.java
@@ -25,7 +25,10 @@ class MenuLayout extends VBox {
         Button bubbleSortBtn = new Button("Bubble Sort");
         bubbleSortBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new BubbleSortLayout()));
 
-        buttonsBox.getChildren().addAll(bubbleSortBtn);
+        Button sudokuBtn = new Button("Sudoku Solver");
+        sudokuBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new Sudoku()));
+
+        buttonsBox.getChildren().addAll(bubbleSortBtn, sudokuBtn);
         getChildren().addAll(title, buttonsBox);
     }
 }
--- Sudoku.java
+++ Sudoku.java
@@ -1,5 +1,41 @@
-public class Sudoku {
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Sudoku extends GridPane{
     Sudoku(){
         
+        int subGridSize = 3;   // Size of subgrid (3x3)
+        int cellSize = 200;     // Size of each cell
+
+
+        // Generate 3x3 grid UI with nested 3x3 cells inside each main cell
+        for (int row = 0; row < subGridSize; row++) {
+            for (int col = 0; col < subGridSize; col++) {
+                
+                GridPane innerGrid = new GridPane();
+                
+                for (int i = 0; i < subGridSize; i++) {
+                    for (int j = 0; j < subGridSize; j++) {
+                        Rectangle innerCell = new Rectangle(cellSize / subGridSize, cellSize / subGridSize);
+                        innerCell.setFill(Color.WHITE);
+                        innerCell.setStroke(Color.LIGHTGRAY);
+                        innerCell.setStrokeWidth(1);
+                        innerGrid.add(innerCell, j, i);
+                    }
+                }
+                
+                Rectangle outerCell = new Rectangle(cellSize, cellSize);
+                outerCell.setFill(Color.TRANSPARENT);
+                outerCell.setStroke(Color.BLACK);
+                outerCell.setStrokeWidth(7);
+
+                StackPane stack = new StackPane();
+                stack.getChildren().addAll(outerCell, innerGrid);
+                add(stack, col, row);
+            }
+        }
+
     }
 }
