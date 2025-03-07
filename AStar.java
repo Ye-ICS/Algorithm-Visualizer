2dd4eae93a9e828545a1ad8ee3c52b10487da431
masa-chau <masa21chau@gmail.com>
2025-03-07 14:05:16 -0500

    Button Functions
    Implemented resetGrid(), removes start/end & walls.
    Implemented "Cell" Class that stores information on location, state (wall, start, end), and flips state if clicked (1st click = start, 2nd click = end, 3 <= wall). Start node = green, end node = red, wall node = black.
    created Cell[][] to store grid dimensions.
    set start/end nodes = null at start.

--- AStar.java
+++ AStar.java
@@ -1,4 +1,3 @@
-
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,14 +14,18 @@ public class AStar extends BorderPane {
     private static final int GRID_SIZE = 40;
     private static final int CELL_SIZE = 20;
 
+    private static Cell[][] grid = new Cell[GRID_SIZE][GRID_SIZE];
+    private static Cell startNode = null;
+    private static Cell endNode = null;
+
     public AStar() {
         GridPane gridPane = new GridPane();
 
         for (int row = 0; row < GRID_SIZE; row++) {
             for (int col = 0; col < GRID_SIZE; col++) {
-                Rectangle cell = new Rectangle(CELL_SIZE, CELL_SIZE, Color.LIGHTGRAY);
-                cell.setStroke(Color.BLACK);
-                gridPane.add(cell, col, row);
+                Cell cell = new Cell(row, col);
+                grid[row][col] = cell;
+                gridPane.add(cell.getRectangle(), col, row);
             }
         }
         
@@ -39,6 +42,80 @@ public class AStar extends BorderPane {
 
         // Back button returns to the main menu
         backButton.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
+        clearButton.setOnAction(event -> resetGrid());
+        aStarButton.setOnAction(event -> RunAStar());
+
     }    
+
+    public static void resetGrid(){
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                grid[row][col].reset();
+            }
+        }
+        startNode = null;
+        endNode = null;
+        System.out.println("Grid Cleared");
+    }
+
+    public static void RunAStar(){
+
+    }
+
+    private class Cell {
+        private int row, col;
+        private Rectangle rect;
+        private boolean isWall = false;
+        private double distance = Double.MAX_VALUE;
+        private Cell parent = null;
+
+        public Cell(int row, int col){
+            this.row = row;
+            this.col = col;
+            this.rect = new Rectangle(CELL_SIZE, CELL_SIZE, Color.LIGHTGRAY);
+            this.rect.setStroke(Color.BLACK);
+
+            rect.setOnMouseClicked(event -> cellClicked());
+        }
+
+        public Rectangle getRectangle(){
+            return rect;
+        }
+
+        public boolean isWall(){
+            return isWall;
+        }
+
+        public void setWall(boolean wall){
+            isWall = wall;
+            if (wall){
+                rect.setFill(Color.BLACK);
+            } else {
+                rect.setFill(Color.LIGHTGRAY);
+            }
+        }
+
+        public void reset() {
+            isWall = false;
+            rect.setFill(Color.LIGHTGRAY);
+            distance = Double.MAX_VALUE;
+            parent = null;
+        }
+
+        private void cellClicked() {
+            if (startNode == null) {
+                startNode = this;
+                rect.setFill(Color.GREEN);
+            } else if (endNode == null) {
+                endNode = this;
+                rect.setFill(Color.RED);
+            } else {
+                setWall(!isWall);
+            }
+        }
+    }
 }
+
+
+
 
