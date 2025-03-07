5c9189104c8b121006cea1a10f2596bc314f3d8e
B1gCD <devlinc@limestone.on.ca>
2025-03-07 12:59:55 -0500

    Basic javaFX code, variable changes

--- EditDistanceC.java
+++ EditDistanceC.java
@@ -1,32 +1,42 @@
 import java.util.Scanner;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 
-public class EditDistanceC {
-    void EditDistancec() {
-        Scanner input = new Scanner(System.in);
-           
+public class EditDistanceC extends FlowPane{
+    EditDistanceC() {
+        Text display = new Text("The result will show up here");
+        Button submitBtn = new Button("Go");
+        submitBtn.setOnAction(event -> display.setText("Distance: " + testEditDistance("hi", "he")));
+
+        getChildren().addAll(display, submitBtn); //Basic structure for testing
+    }
+
+
+    int testEditDistance(String string, String otherString) {
         //Backend only for now for testing
         System.out.println("Welcome! Please input string number 1 you want to compare:");
-        String input1 = input.nextLine();
-        String[] comp1; 
-        comp1 = new String[input1.length()];
+
+        String[] compOne; 
+        compOne = new String[string.length()];
 
         System.out.println("Please input string number 2 you want to compare:");
-        String input2 = input.nextLine();
-        String[] comp2; 
-        comp2 = new String[input2.length()]; 
+
+        String[] compTwo; 
+        compTwo = new String[otherString.length()]; 
 
 
         //Fill each array index[i] with corresponding letter in index[i] from string input1
-        for (int i = 0; i != input1.length(); i++) {
-            comp1[i] = input1.substring(i);
+        for (int i = 0; i != string.length(); i++) {
+            compOne[i] = string.substring(i);
         } 
         //Same thing but for second array - for future use in ease of comparing.
-        for (int i = 0; i != input2.length(); i++) {
-            comp2[i] = input2.substring(i);
+        for (int i = 0; i != otherString.length(); i++) {
+            compTwo[i] = otherString.substring(i);
         } 
         System.out.println("Calculating...");
 
-
+        return 0;
 
     }
 }
