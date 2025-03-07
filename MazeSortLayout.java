47238cf253b5315f354ed881bc09d2cc9ae00a3d
Declan-gaelen <pm@declanjones.ca>
2025-03-07 12:23:27 -0500

    Create MazeSortLayout file

---
+++ MazeSortLayout.java
@@ -0,0 +1,9 @@
+import javafx.scene.layout.FlowPane;
+import javafx.scene.image.Image; 
+import javafx.scene.image.ImageView;
+
+public class MazeSortLayout extends FlowPane{
+    MazeSortLayout() {
+        
+    }
+}
