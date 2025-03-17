d2894744b4273ae966c9c56734c526b5a533670a
Gabriel Djordjevich <gdj.fireball@gmail.com>
2025-03-17 12:40:32 -0400

    Finish values generation
    Finished the generation and display of labels containing random values from 1 - 500.

--- VisualSortingAlgorithms.java
+++ VisualSortingAlgorithms.java
@@ -6,34 +6,39 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.canvas.GraphicsContext;
 
 public class VisualSortingAlgorithms extends BorderPane {
     VisualSortingAlgorithms() {
-        HBox infoBox = new HBox();
         VBox selectionBox = new VBox();
         HBox displayBox = new HBox();
-        HBox buttonBox = new HBox();
 
         Label valueLabel = new Label("Number of values:");
-        Spinner<Integer> valueSpinner = new Spinner<Integer>(1, 25, 1);
+
+        Spinner<Integer> valueSpinner = new Spinner<Integer>(2, 25, 1);
         valueSpinner.setEditable(true);
 
-        int numSwaps;
-        int[] values = new int[10];
+        // int numSwaps;
 
+        Button submitBtn = new Button();
+        submitBtn.setText("Submit");
+        submitBtn.setOnAction(event -> calculateValues(displayBox, valueSpinner));
+
+        setLeft(selectionBox);
+        setCenter(displayBox);
+
+        selectionBox.getChildren().addAll(valueLabel, valueSpinner, submitBtn);
+    }
+
+    void calculateValues(HBox displayBox, Spinner<Integer> valueSpinner) {
+        int[] values = new int[valueSpinner.getValue()];
+        displayBox.getChildren().clear();
         Label[] valueLabels = new Label[values.length];
         for (int i = 0; i < values.length; i++) {
             valueLabels[i] = new Label();
             values[i] = (int) (500 * Math.random()) + 1;
             displayBox.getChildren().add(valueLabels[i]);
-            valueLabels[i].setText("[" + values[i] + "] ");
+            valueLabels[i].setText("  " + values[i] + "  ");
         }
-
-        setTop(infoBox);
-        setLeft(selectionBox);
-        setCenter(displayBox);
-        setBottom(buttonBox);
-
-        selectionBox.getChildren().addAll(valueLabel, valueSpinner);
     }
 }
