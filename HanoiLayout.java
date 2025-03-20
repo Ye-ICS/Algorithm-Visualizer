b6563de753da2838e0c635040d489abe415d4359
Maher <157660848+MaherHssn@users.noreply.github.com>
2025-03-20 01:08:06 -0400

    Numba 2
    added level spinner, solve button, towers themselves

--- HanoiLayout.java
+++ HanoiLayout.java
@@ -1,20 +1,46 @@
 import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
-/**
- * Placeholder. May delete later.
- */
-public class HanoiLayout extends FlowPane{
+public class HanoiLayout extends FlowPane {
     HanoiLayout() {
         setAlignment(Pos.CENTER);
 
-        Text description = new Text("HIIII!I!I!I!I!");
+        Text description = new Text("Tower of Hanoi");
+
+        Canvas canvas = new Canvas(600, 200); 
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //base of tower things
+        gc.setFill(Color.GRAY);
+        gc.fillRect(50, 150, 100, 10); 
+        gc.fillRect(250, 150, 100, 10);
+        gc.fillRect(450, 150, 100, 10);
+
+        //middle sticks of tower things
+        gc.setFill(Color.GRAY);
+        gc.fillRect(95, 50, 10, 100);
+        gc.fillRect(295, 50, 10, 100);
+        gc.fillRect(495, 50, 10, 100);
+
+        Spinner<Integer> levelSpinner = new Spinner<Integer>(0, 100000, 0, 1);
+        levelSpinner.setEditable(true);
+
 
         Button backBtn = new Button("Back");
         backBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
 
-        getChildren().addAll(description, backBtn);
+        Button solveBtn = new Button("solve");
+        //solveBtn.setOnAction(event -> method i make in future :D);
+
+
+        getChildren().addAll(description, canvas, backBtn, solveBtn, levelSpinner);
     }
+
 }
