7e58388cb81c5d76a8e62efc1e058406395077fb
man_writing_sudoku <>
2025-03-22 15:12:28 -0400

    added backtracing, and checking for row column and sqaure

---
+++ SudokuSolver.java
@@ -0,0 +1,149 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+public class SudokuSolver extends FlowPane{
+
+    
+    SudokuSolver() {
+            GridPane grid = new GridPane();
+
+        
+        int[][] board = new int[9][9];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                board[i][j] = 0;
+            }
+        }
+
+        board[0][0] = 5;
+        board[0][3] = 8;
+        board[0][4] = 3;
+        board[0][5] = 2;
+        board[0][6] = 4;
+        board[0][8] = 6;
+        board[1][1] = 6;
+        board[1][2] = 3;
+        board[1][3] = 7;
+        board[1][4] = 4;
+        board[2][0] = 8;
+        board[2][2] = 2;
+        board[2][3] = 1;
+        board[2][4] = 9;
+        board[2][8] = 3;
+        board[3][1] = 3;
+        board[3][4] = 2;
+        board[3][5] = 9;
+        board[3][6] = 1;
+        board[3][8] = 5;
+        board[4][0] = 1;
+        board[4][5] = 8;
+        board[4][6] = 9;
+        board[4][7] = 6;
+        board[4][8] = 2;
+        board[5][3] = 5;
+        board[5][7] = 7;
+        board[6][4] = 1;
+        board[6][7] = 2;
+        board[6][8] = 7;
+        board[7][1] = 2;
+        board[7][2] = 6;
+        board[7][6] = 5;
+        board[8][0] = 3;
+        board[8][1] = 1;
+        board[8][2] = 8;
+        board[8][4] = 5;
+        board[8][7] = 4;
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != 0){
+                    grid.add(new Text("" + board[i][j]), i, j);
+                }
+                else{
+                    grid.add(new Text("_"), i, j);
+                }
+            }
+        }
+        grid.setVgap(10);
+        grid.setHgap(15);
+
+        setAlignment(Pos.CENTER);
+        Button backBtn = new Button("Back");
+        Button start = new Button("start");
+        start.setOnAction(event -> solve(board));
+        backBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
+
+        getChildren().addAll(grid, backBtn, start);
+
+    }
+    void solve(int board[][]){
+        int x = 0;
+        int y = 0;
+        int coordinate = 0;
+        boolean backtracking = false;
+        boolean condition = true;
+        while(condition){
+            System.out.println("step " + coordinate);
+            if(backtracking){
+                System.out.println("backtracking");
+            }
+            x = coordinate % 9;
+            y = coordinate / 9;
+            if(board[x][y] <= 0){
+                int previous = board[x][y];
+                board[x][y] = 0;
+                backtracking = true;
+               for(int i = Math.abs(board[x][y]) + 1; i <= 9; i++){
+                   if(check(board, x, y, i)){
+                       board[x][y] = -i;
+                       backtracking = false;
+                       if(coordinate == 80){
+                           System.out.println("done\n");
+                            condition = false;
+                       }
+                   }
+               }
+               if(board[x][y] == previous){
+                   backtracking = true;
+               }
+
+            }
+            if(backtracking){
+                coordinate-=2;
+            }
+            coordinate++; 
+        }
+        
+
+    }
+    private static boolean check(int[][] board, int x, int y, int num){
+        boolean provenFalse = false;
+        for(int i = 0; i < 9; i++){
+            if(Math.abs(board[i][x]) == num || Math.abs(board[y][i]) == num || boxContains(board, x, y, num)){
+                provenFalse = true;
+                break;
+            }
+        }
+
+        if(provenFalse){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean boxContains(int[][] board, int x, int y, int num){
+        // board[x][y]
+        for(int i = 0; i < 9; i++){
+            if(Math.abs(board[x/3 * 3 + i % 3][y/3 * 3 + i / 3]) == num){
+                return true;
+            }
+        }
+        return false;
+
+    }
+    
+}
