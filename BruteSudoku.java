aab4448f61fe8f610680e4f4b65581f6321ce203
NoahE321 <ne6mq07@limestone.on.ca>
2025-03-25 11:55:01 -0400

    Update BruteSudoku.java

--- BruteSudoku.java
+++ BruteSudoku.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -8,17 +7,13 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
-public class BruteSudoku extends FlowPane{
+public class BruteSudoku extends FlowPane {
     
     TextField[][] sudokuCells = new TextField[9][9];
-     Random random = new Random();
-     Boolean noSolution = false;
+    Random random = new Random();
 
-    /**
-     * Constructs layout for brute force sudoku solver
-     */
+
     BruteSudoku() {
-
         Text title = new Text("Brute Force Sudoku Solver");
 
         GridPane sudokuTable = new GridPane();
@@ -34,8 +29,6 @@ public class BruteSudoku extends FlowPane{
                 sudokuNumber.setPrefHeight(40);
                 sudokuNumber.setPrefWidth(40);
                 sudokuNumber.setAlignment(Pos.CENTER);
-
-
                 sudokuCells[i][j] = sudokuNumber;
                 sudokuTable.add(sudokuNumber, i, j);
             }
@@ -45,131 +38,117 @@ public class BruteSudoku extends FlowPane{
 
         fillRandomNumbers();
 
-      
-
-        Text description = new Text("");
-
-        Button sudokuBox = new Button("");
-
         Button solveButton = new Button("Solve");
-        
+        solveButton.setOnAction(event -> solveSudoku());
 
         Button backsBtn = new Button("Back");
         backsBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
 
-        solveButton.setOnAction(event -> solveSudoku());
-
         getChildren().addAll(title, backsBtn, sudokuTable, solveButton);
     }
 
-    void BruteForceNumbers() {
+
+    void solveSudoku() {
         int[][] board = new int[9][9];
 
+       
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 String text = sudokuCells[i][j].getText().trim();
                 if (!text.isEmpty() && text.matches("[1-9]")) {
                     board[i][j] = Integer.parseInt(text);
                 } else {
-                    board[i][j] = 0; 
+                    board[i][j] = 0;
                 }
             }
         }
 
-
-
-
-}
-
-void solveSudoku() {
-    int[][] board = new int[9][9];
-
-    // 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            String text = sudokuCells[i][j].getText().trim();
-            if (!text.isEmpty() && text.matches("[1-9]")) {
-                board[i][j] = Integer.parseInt(text);
-            } else {
-                board[i][j] = 0;
-            }
-        }
-    }
-
-    // Solve the Sudoku
-    if (solve(board)) {
         
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                sudokuCells[i][j].setText(String.valueOf(board[i][j]));
-            }
-        }
-    } else {
-        System.out.println("No solution exists.");
-        fillRandomNumbers();
-    }
-}
-
-// 
-boolean solve(int[][] board) {
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
-            if (board[row][col] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    if (isValid(board, row, col, num)) {
-                        board[row][col] = num;
-                        if (solve(board)) {
-                            return true;
-                        }
-                        board[row][col] = 0; // 
-                    }
+        if (solve(board)) {
+            
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    sudokuCells[i][j].setText(String.valueOf(board[i][j]));
                 }
-                return false; // No valid number found
+            }
+        } else {
+            System.out.println("No solution exists.");
+            fillRandomNumbers();
+        }
+    }
+
+
+    boolean solve(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0; 
+                        }
+                    }
+                    return false; // No valid number found
+                }
             }
         }
-    }
-    return true; // Solved
-}
-
-//
-boolean isValid(int[][] board, int row, int col, int num) {
-    for (int i = 0; i < 9; i++) {
-        if (board[row][i] == num || board[i][col] == num) {
-            return false;
-        }
+        return true; 
     }
 
-    // 
-    int startRow = (row / 3) * 3;
-    int startCol = (col / 3) * 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[startRow + i][startCol + j] == num) {
+    boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
                 return false;
             }
         }
+
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
-    return true;
-}
 
     void fillRandomNumbers() {
         clearTable();
+        int[][] board = new int[9][9];
 
-        int numbersToPlace = random.nextInt(20) + 10;
+        solve(board);
 
-        for (int k = 0; k < numbersToPlace; k++) {
+
+        int numbersToRemove = 50; 
+        for (int k = 0; k < numbersToRemove; k++) {
             int i, j;
             do {
                 i = random.nextInt(9);
                 j = random.nextInt(9);
-            } while (!sudokuCells[i][j].getText().isEmpty());
+            } while (board[i][j] == 0);
 
-            int randomNum = random.nextInt(9) + 1;
-            sudokuCells[i][j].setText(String.valueOf(randomNum));
-            sudokuCells[i][j].setEditable(false);
+            board[i][j] = 0;
+        }
+
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != 0) {
+                    sudokuCells[i][j].setText(String.valueOf(board[i][j]));
+                    sudokuCells[i][j].setEditable(false);
+                }
+            }
         }
     }
+
 
     void clearTable() {
         for (int i = 0; i < 9; i++) {
