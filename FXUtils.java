47ba63b615db64dcd9b7bd293e36bcece519a688
ShadowJaguar1 <willdelouche3@gmail.com>
2025-04-17 13:34:51 -0400

    removed useless drawLine method from FXUtils

--- FXUtils.java
+++ FXUtils.java
@@ -9,19 +9,14 @@ import javafx.scene.canvas.GraphicsContext;
 public class FXUtils {
     /**
      * Sets new root node for scene, auto-resizing to fit
-     * @param scene Scene to set root for
+     * 
+     * @param scene    Scene to set root for
      * @param rootNode New root node
      */
     static void setSceneRoot(Scene scene, Parent rootNode) {
         scene.setRoot(rootNode);
-        scene.getWindow().sizeToScene();    // Auto-resize
+        scene.getWindow().sizeToScene(); // Auto-resize
     }
-    
-    // TODO: Make a method for creating layout switching buttons
 
-    static void drawLine(Canvas canvas, int x1, int y1, int x2, int y2) {
-        // draw a line
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.strokeLine(x1, y1, x2, y2);
-    }
+    // TODO: Make a method for creating layout switching buttons
 }
