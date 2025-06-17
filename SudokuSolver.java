bacacc330599ffaa0b30eb1e665e0ef1e5d13b28
Base Crown <>
2025-06-17 12:27:04 -0400

    removed superfluous files

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,13 +1,25 @@
+import javax.swing.JSlider;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
+// note the  negative numbers are to distinguish the difference between set values and potential solutions.
 public class SudokuSolver extends FlowPane{
 
-    
     SudokuSolver() {
             GridPane grid = new GridPane();
+         
+
 
         
         int[][] board = new int[9][9];
@@ -63,12 +75,7 @@ public class SudokuSolver extends FlowPane{
         TextField[][] fields = new TextField[9][9];
 
         int delay = 50;
-        //Timeline timeline = new Timeline();
-       // KeyFrame keyframe = new KeyFrame(Duration.millis(delay), event -> solve(board, ){
-         //   var result = solve(null);
-            
-       // });
-
+        // generating text boxes
 
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
@@ -80,9 +87,18 @@ public class SudokuSolver extends FlowPane{
                 else{
                     fields[i][j] = new TextField("_");
                     grid.add(fields[i][j], i, j);
+                    
                 }
+                fields[i][j].setMaxWidth(40.0);
             }
         }
+        Slider slider = new Slider(1, 100, 50);
+        slider.setShowTickMarks(true);  // Show actual ticks
+        slider.setMajorTickUnit(1);     // Major tick spacing
+        slider.setBlockIncrement(1);   // Increment per key press
+        
+
+        // Add listener to update TextField when the slider value changes
         grid.setVgap(10);
         grid.setHgap(15);
 
@@ -90,14 +106,31 @@ public class SudokuSolver extends FlowPane{
         Button backBtn = new Button("Back");
         Button start = new Button("start");
         Button resetBtn = new Button("reset");
-        start.setOnAction(event -> {updateBoard(board, fields); solve(board, 0,fields);});
-        backBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
-        resetBtn.setOnAction(event -> reset(board, fields));
+        Text speed_text = new Text("Speed!!!");
+        VBox speed = new VBox();
 
-        getChildren().addAll(grid, backBtn, start, resetBtn);
+        speed.getChildren().addAll(speed_text, slider);
+
+        start.setOnAction(event -> {
+                    Thread work = new Thread(() -> {
+            solve(board, gCoordinate, fields, slider);
+         });
+            updateBoard(board, fields); work.start();;});
+        backBtn.setOnAction(event -> FXUtils.setSceneRoot(getScene(), new MenuLayout()));
+        resetBtn.setOnAction(event -> {reset(board, fields);;});
+
+        getChildren().addAll(grid, backBtn, start, resetBtn, speed);
 
     }
-    boolean solve(int board[][],int coordinate, TextField[][] fields){
+    //Main function
+    boolean solve(int board[][],int coordinate, TextField[][] fields, Slider slider){
+        updateGrid(board, fields);
+        try {
+            // made so that right is faster
+            Thread.sleep(3000/(long)slider.getValue());
+        } catch (InterruptedException e) {
+            e.printStackTrace(); 
+        }
         System.out.println("coordinate: " + coordinate);
         int x = coordinate / 9;
         int y = coordinate % 9;
@@ -111,14 +144,12 @@ public class SudokuSolver extends FlowPane{
         for(int i = 1; i <= 9; i++){
             if(check(board, coordinate / 9, coordinate % 9, i)){
                 board[x][y] = -i;
-                updateGrid(board, fields);
                 if(coordinate == 80){
-                    updateGrid(board, fields);
                     System.out.println("done");
                     return true;
                 }
-                
-                if(solve(board, new_coordinate, fields)){
+                //for if its all done
+                if(solve(board, new_coordinate, fields, slider)){
                     return true;
                 }
             }
@@ -129,21 +160,16 @@ public class SudokuSolver extends FlowPane{
         }
         return false;
     }
+    //checking if the board posistion is valid
     private static boolean check(int[][] board, int x, int y, int num){
-        boolean provenFalse = false;
         for(int i = 0; i < 9; i++){
             if(Math.abs(board[x][i]) == num || Math.abs(board[i][y]) == num || boxContains(board, x, y, num)){
-                provenFalse = true;
-                break;
+                return false;
             }
-        }
-
-        if(provenFalse){
-            return false;
         }
         return true;
     }
-
+    //checks for others of the same number in the same 3x3 grid
     private static boolean boxContains(int[][] board, int x, int y, int num){
         for(int i = 0; i < 9; i++){
             if(Math.abs(board[x/3 * 3 + i / 3][y/3 * 3 + i % 3]) == num){
@@ -161,25 +187,31 @@ public class SudokuSolver extends FlowPane{
             }
         }
     }
-    private static void updateGrid(int[][] board, TextField[][] fields){
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(Math.abs(board[i][j]) > 0){
-                    fields[i][j].setText("" + Math.abs(board[i][j]));
+private static void updateGrid(int[][] board, TextField[][] fields) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int value = Math.abs(board[i][j]);
+            TextField tf = fields[i][j];  // this is effectively final
+            System.out.print(value);
+            if (j == 8) System.out.println();
+
+            Platform.runLater(() -> {
+                if (value > 0) {
+                    tf.setText("" + value);
+                } else {
+                    tf.setText("_");
                 }
-                else{
-                    fields[i][j].setText("_");
-                }
-            }
+            });
         }
     }
+}
     private static void updateBoard(int[][] board, TextField[][] fields){
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 try{
                     board[i][j] = Integer.parseInt(fields[i][j].getText());
                 }
-                catch(Exception e){
+                catch(Exception NumberFormatException){
                     board[i][j] = 0;
                 }
 
